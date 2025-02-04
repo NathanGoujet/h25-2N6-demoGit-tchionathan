b6b1065a94449bb0d8f3c9e2ab44a6023daa903c
NathanGoujet <6297003@cegepmontpetit.ca>
2025-02-04 15:00:58 -0500

    Update Ex8 csv liste et for.py

--- R03 Exercices Depart/Ex8 csv liste et for.py
+++ R03 Exercices Depart/Ex8 csv liste et for.py
@@ -7,7 +7,8 @@ import csv
  
 os.chdir("C:\\Users\\6297003\\Documents\\GitHub\\h25-2N6-demoGit-tchionathan\\R03 Exercices Depart\\csvs\\")
 # Vous utiliserez encore le fichier "Ex7 Lan Party.csv"
-# 
+
+#  
 #         Créez une liste des jeux joués dans les différents Lan Party du fichier.
 #         N'ajoutez à cette liste chaque jeu qu'une seule fois 
 #                         (vérifiez si le jeu est dans la liste avant de l'ajouter
@@ -42,11 +43,18 @@ os.chdir("C:\\Users\\6297003\\Documents\\GitHub\\h25-2N6-demoGit-tchionathan\\R0
 
 # INSTRUCTIONS DÉTAILLÉES
 #      Commencez par créer une liste des différents jeux vide
+jeux = []
 #      Ouvrez le fichier 'Ex4 Lan Party.csv' en lecture
+with open("Ex7 Lan Party.csv", 'r', encoding='UTF-8') as csv_read:  
 #      utilisez csv.reader pour lire le fichier avec le bon delimiter
+    csv_read = csv.reader(csv_read,
+                          delimiter=';')
 #      Sautez la première ligne de l'entête
+    next()
 #      Faites une boucle pour passer à travers chacune des lignes 
+    for ligne in csv_read:
 #      Utilisez le slicing pour garder uniquement les 3 jeux
+        print()
 #      Faites une boucle pour passer à travers chacun des jeux
 #      Avec count, obtenez le nombre de fois que le jeu est dans votre liste des différents jeux
 #      Si le count est de 0, vous ne l'avez jamais ajouté alors ajoutez le jeu à votre liste
